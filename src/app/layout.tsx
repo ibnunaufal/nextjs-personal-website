230f30a2fb91a9d1ead822a738ffbc85c8759a37
ibnu naufal <ibnunaufal5758@gmail.com>
2023-08-15 10:49:23 +0700

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#fff" />
       </head>
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
